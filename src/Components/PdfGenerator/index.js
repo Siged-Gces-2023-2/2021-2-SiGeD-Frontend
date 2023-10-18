dd1bd2db594159c4df842dd7d84e949af03cba1b
Guilherme <sofiabrasil44@gmail.com>
2023-10-17 23:00:28 -0300

    #40 - add criação de PDF, ajustes gerais e inicializando fontes

--- src/Components/PdfGenerator/index.js
+++ src/Components/PdfGenerator/index.js
@@ -1,59 +1,16 @@
 import * as pdfMake from 'pdfmake/build/pdfmake';
-import 'pdfmake/build/vfs_fonts';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
 const generatePatrimonyPDF = (patrimonios) => {
-  const documentstyles = {
+  pdfMake.vfs = pdfFonts.pdfMake.vfs;
+  const documentStyles = {
     header: {
-      fontSize: 30,
+      fontSize: 20,
       bold: true,
       alignment: 'center',
     },
-    subTitle: {
-      fontSize: 22,
-      bold: true,
-      alignment: 'center',
-      decoration: 'underline',
-    },
-    filterTitle: {
-      fontSize: 16,
-    },
-    filterStyle: {
-      alignment: 'left',
-    },
-    dateStyle: {
-      alignment: 'right',
-    },
-    leftAlign: {
-      alignment: 'left',
-    },
-    title: {
-      bold: true,
-    },
-    demandTitle: {
-      fontSize: 16,
-      marginTop: 30,
-      bold: true,
-    },
-    important: {
-      color: '#FF0000',
-    },
-    subTitleLeft: {
-      fontSize: 14,
-      bold: true,
-      alignment: 'center',
-    },
-    sessionStyle: {
-      background: '#ccc',
-    },
-    tableTitle: {
-      bold: true,
-      alignment: 'center',
-    },
-    tableLeftInfo: {
-      alignment: 'center',
-    },
-    defaultStyle: {
-      alignment: 'justify',
+    subheader: {
+      color: 'black',
     },
   };
 
@@ -66,13 +23,13 @@ const generatePatrimonyPDF = (patrimonios) => {
       ...patrimonios.map((patrimonio) => [
         { text: `Nome: ${patrimonio.name}`, style: 'subheader' },
         { text: `Descrição: ${patrimonio.description}` },
-        { text: '------------------------' },
+        { text: '--------------------------------------------------------------------------------------------------------------------------------------------------' },
         { text: '\n' },
       ]),
     ],
+    styles: documentStyles,
   };
-
-  pdfMake.createPdf(documentDefinition.content, documentstyles).print();
+  pdfMake.createPdf(documentDefinition).print();
   return (null);
 };
 
